56409c70f4db85cba4a1a9d70113ca715af7708e
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-05-25 09:22:06 -0700

    ECR repository creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,22 @@ locals {
     Environment  = var.APP_ENV
   }
 }
+
+resource "aws_ecr_repository" "cd_repo" {
+  name = "${var.APP_ORG}/${var.APP_NAME}-${var.APP_ENV}"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+
+  tags = local.tags
+}
+
 resource "aws_ecs_task_definition" "app_task_definition" {
   family                = local.app_id
   container_definitions = data.template_file.console_task_definition.rendered
