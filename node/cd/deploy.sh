a37c61ee947bab32926176b00edce37cef7a2c09
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-06-01 11:32:54 -0700

    Adding absolute path and ECR repo creation

--- node/cd/deploy.sh
+++ node/cd/deploy.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
-XTAGES_APP_ENV=$1
-NODE_VERSION=$2
-XTAGES_ORG=$3
-APP_TAG=$4
+PROJECT_TYPE=$1
+XTAGES_APP_ENV=$2
+NODE_VERSION=$3
+XTAGES_ORG=$4
+APP_TAG=$5
 # this path is inferred from the buildspec file that is in S3 repo tf_live_production
-PROJECT_PATH=../../../project_src
-cp Dockerfile $PROJECT_PATH
-cd $PROJECT_PATH || exit
-XTAGES_PROJECT=$(basename -s .git "$(git config --get remote.origin.url)")
+PROJECT_PATH=/project_src
+RECIPES_BASE_PATH=/deploy_src
 
+# Terraform ECR repo creation if needed
+cd "${RECIPES_BASE_PATH}"/terraform/ecr || exit
+terraform init \
+  -backend-config "key=tfstate/us-east-1/production/${TF_VAR_APP_ORG}/${TF_VAR_APP_NAME}/app/${TF_VAR_APP_ENV}"
+terraform plan && terraform apply -auto-approve
+
+cd "${RECIPES_BASE_PATH}/${PROJECT_TYPE}"/cd || exit
+cp Dockerfile ${PROJECT_PATH}
+cd "${PROJECT_PATH}" || exit
+XTAGES_PROJECT="$(basename -s .git "$(git config --get remote.origin.url)")"
+
+cd "${PROJECT_PATH}"|| exit
 # build docker image and push it to ECR
 # docker login is performed in the buildspec (S3)
 docker build --build-arg NODE_VERSION="${NODE_VERSION}" \
---tag 606626603369.dkr.ecr.us-east-1.amazonaws.com/"${XTAGES_ORG}"/"${XTAGES_PROJECT}-${XTAGES_APP_ENV}:${APP_TAG}" .
+  --tag 606626603369.dkr.ecr.us-east-1.amazonaws.com/"${XTAGES_ORG}"/"${XTAGES_PROJECT}-${XTAGES_APP_ENV}:${APP_TAG}" .
 docker push 606626603369.dkr.ecr.us-east-1.amazonaws.com/"${XTAGES_ORG}"/"${XTAGES_PROJECT}-${XTAGES_APP_ENV}:${APP_TAG}"
 
 # deploy to ECS with Terraform
-(cd -) && (cd ../../terraform || exit)
-export TF_VAR_TAG=$APP_TAG
+cd "${RECIPES_BASE_PATH}"/terraform || exit
+export TF_VAR_TAG="$APP_TAG"
 export TF_VAR_APP_NAME="${XTAGES_PROJECT}"
 export TF_VAR_APP_ENV="${XTAGES_APP_ENV}"
 export TF_VAR_APP_ORG="${XTAGES_ORG}"
--- terraform/main.tf
+++ terraform/main.tf
@@ -7,21 +7,6 @@ locals {
   }
 }
 
-resource "aws_ecr_repository" "deploy_app_repo" {
-  name                 = "${var.APP_ORG}/${var.APP_NAME}-${var.APP_ENV}"
-  image_tag_mutability = "IMMUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  encryption_configuration {
-    encryption_type = "KMS"
-  }
-
-  tags = local.tags
-}
-
 resource "aws_ecs_task_definition" "app_task_definition" {
   family                = local.app_id
   container_definitions = data.template_file.app_task_definition.rendered
