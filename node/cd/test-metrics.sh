913f9f7c288d1097fa4291a26571a8589ce391e5
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-08-31 10:08:47 -0700

    changing file name when it's uploaded

--- node/cd/test-metrics.sh
+++ node/cd/test-metrics.sh
@@ -1,7 +1,12 @@
-SCRIPT_DIR=$(dirname "${0}")
-RECIPES_BASE_PATH="$PWD"
-SCRIPTS_PATH="${RECIPES_BASE_PATH}/${SCRIPT_DIR}"/
-
-echo "reporting metric for docker"
-XTAGES_ORG="ad85be32aa233b7226d34a91cf66b044" XTAGES_PROJECT="6fa68232c5ec330744b2004ac7da774d" XTAGES_APP_ENV="staging" sh -x "${SCRIPTS_PATH}"/metrics.sh "docker" "1" "build=start"
-echo  "reported"
+#SCRIPT_DIR=$(dirname "${0}")
+#RECIPES_BASE_PATH="$PWD"
+#SCRIPTS_PATH="${RECIPES_BASE_PATH}/${SCRIPT_DIR}"/
+#
+#echo "reporting metric for docker"
+#XTAGES_ORG="ad85be32aa233b7226d34a91cf66b044" XTAGES_PROJECT="6fa68232c5ec330744b2004ac7da774d" XTAGES_APP_ENV="staging" sh -x "${SCRIPTS_PATH}"/metrics.sh "docker" "1" "build=start"
+#echo  "reported"
+shopt -s nullglob
+for file in *.sh
+do
+  basename "${file}"
+done
--- node/cd/upload_logs.sh
+++ node/cd/upload_logs.sh
@@ -6,7 +6,8 @@ S3_URI="s3://${S3_LOGS_BUCKET}/logs/us-east-1/${XTAGES_ORG_HASH}/${XTAGES_PROJEC
 shopt -s nullglob
 for file in "${SCRIPTS_PATH}"/*.log
 do
-  mv "${file}" "${STATUS_CODE}_${file}"
-  aws s3 mv "${STATUS_CODE}_${file}" "${S3_URI}/${STATUS_CODE}_${file}"
+  RENAMED_FILE="$(dirname "${file}")/${STATUS_CODE}-$(basename "${file}")"
+  mv "${file}" "${RENAMED_FILE}"
+  aws s3 mv "${RENAMED_FILE}" "${S3_URI}/${STATUS_CODE}-$(basename "${file}")"
 done
 
