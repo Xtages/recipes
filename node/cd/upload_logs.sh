327a253f558c7c8d1b5e86e838f587338b1b6a2e
Matias Dell Amerlina <mdellamerlina@xtages.com>
2021-09-01 14:37:35 -0700

    making S3 quiet

--- node/cd/upload_logs.sh
+++ node/cd/upload_logs.sh
@@ -6,9 +6,8 @@ S3_URI="s3://${S3_LOGS_BUCKET}/logs/us-east-1/${XTAGES_ORG_HASH}/${XTAGES_PROJEC
 shopt -s nullglob
 for file in "${SCRIPTS_PATH}"/*.log
 do
-  echo "moving ${file} to S3"
   RENAMED_FILE="$(dirname "${file}")/${STATUS_CODE}-$(basename "${file}")"
   mv "${file}" "${RENAMED_FILE}"
-  aws s3 mv "${RENAMED_FILE}" "${S3_URI}/${STATUS_CODE}-$(basename "${file}")"
+  aws s3 mv "${RENAMED_FILE}" "${S3_URI}/${STATUS_CODE}-$(basename "${file}")" --quiet
 done
 
