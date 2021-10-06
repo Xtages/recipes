0af9ea867b05e58b664658b47d241f7407be1f85
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-10-05 18:38:39 -0700

    removing ORG var from different places (#37)

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -18,10 +18,6 @@ variable "APP_ENV" {
   description = "Environment where the application will be deploy. This values needs to be defined as an environment variable"
 }
 
-variable "APP_ORG" {
-  description = "Organization that owns the app. This values needs to be defined as an environment variable"
-}
-
 variable "APP_ORG_HASH" {
   description = "Hash that identify the Organization"
 }
