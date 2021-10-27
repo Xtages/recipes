72b5fece7a9d71e8af77f208237305181c441458
Matias Dell Amerlina <79473194+mdellamerlina@users.noreply.github.com>
2021-10-27 11:29:22 -0700

    Free tier changes to deploy to prod (#40)
    * free tier deploys to prod
    
    * removing scale-in for staging

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -22,28 +22,3 @@ resource "aws_cloudwatch_log_group" "app_ecs_log_group" {
   name = "/ecs/${var.APP_NAME_HASH}-${var.APP_ENV}"
   tags = local.tags
 }
-
-resource "aws_appautoscaling_target" "ecs_staging_lower_capacity" {
-  count              = var.APP_ENV == "staging" ? 1 : 0
-  max_capacity       = 1
-  min_capacity       = 0
-  resource_id        = "service/${local.ecs_cluster_name}/${aws_ecs_service.xtages_app_service.name}"
-  scalable_dimension = "ecs:service:DesiredCount"
-  service_namespace  = "ecs"
-}
-
-resource "aws_appautoscaling_scheduled_action" "ecs_staging_lower_capacity_scheduled" {
-  count              = var.APP_ENV == "staging" ? 1 : 0
-  name               = local.app_id
-  service_namespace  = aws_appautoscaling_target.ecs_staging_lower_capacity[0].service_namespace
-  resource_id        = aws_appautoscaling_target.ecs_staging_lower_capacity[0].resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_staging_lower_capacity[0].scalable_dimension
-  schedule           = "cron(${local.min_utc} ${local.hour_utc} ${local.day_utc} ${local.month_utc} ? ${local.year_utc})"
-
-  scalable_target_action {
-    min_capacity = 0
-    max_capacity = 0
-  }
-
-  depends_on = [aws_ecs_service.xtages_app_service]
-}
